feae453a50fe65abf5a90a560317b7a1cd2e96d2
yosizumi1919 <kplmg2030@gmail.com>
2025-02-02 16:36:46 +0900

    デプロイ③

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -56,7 +56,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_new
-    unless current_user == @prototype.user 
+    unless current_user == @prototypes.user 
       redirect_to root_path
     end
   end
